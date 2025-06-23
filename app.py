c40adc1403515ac77127cab12d7c162a9a5b4bc4
S Sreelakshmi <24f3002284@ds.study.iitm.ac.in>
2025-06-24 00:09:38 +0530

    Commit on-24-06-2025

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 #beginning of app
+
+from flask import Flask #importing library
+
+app=None
+
+def setup_app():
+    app=Flask(__name__) #initialisation
+    #sqlite connection-pending
+    app.app_context().push() #to access other modules
+    app.debug=True
+
+setup_app()
+
+from backend.controllers import *
+
+if __name__=='__main__':
+    app.run() #running the app. debug=True=>displays the errors
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,29 @@
 #Data models
+
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+#First entity
+class User_Details(db.model):
+    __tablename__="user_details" #by default,tablename will be taken as the class name with all letters in lower case
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String,unique=True,nullable=False)
+    password=db.Column(db.String,nullable=False)
+    role=db.Column(db.Integer,default=1) #by default, the one who registers is user and not admin
+    full_name=db.Column(db.String,nullable=False)
+    address=db.Column(db.String,nullable=False)
+    pin_code=db.Column(db.Integer,nullable=False)
+
+    #relations..write later
+
+class Parking_lot(db.model):
+    __tablename__="parking_lot"
+    id=db.Column(db.Integer,primary_key=True)
+    address=db.Column(db.String,nullable=False)
+    price=(db.Integer,nullable=False)
+    pin_code=db.Column(db.Integer,nullable=False)
+    maximum_number_of_spots=db.Column(db.Integer,nullable=False)
+    
+
+class 
